b4bb90855757decea226f7139845f0da8aa6de5a
ZhivkoMarinov <79455283+ZhivkoMarinov@users.noreply.github.com>
2025-12-15 14:41:21 +0200

    Add files via upload

---
+++ Bell_Roulette_White/Bell_Roulette_White.ino
@@ -0,0 +1,338 @@
+#include "Bell_roulette_white.h"
+
+wheel wheelControl = {
+    .isWheelRotating = false,
+    .isBallThere = false,
+    .isWheelUp = false,
+    .isRetrieved = false,
+    .error = false,
+};
+
+int roundAttemptsToRetrieve = 0;
+unsigned long roundCounter = 1;
+void(* resetFunc) (void) = 0;
+
+void setup() {
+  singleDelay(powerOnDelay);
+  pinMode(winSensorPin, INPUT_PULLUP);
+  pinMode(wheelSensorPin, INPUT_PULLUP);
+  pinMode(controlWheelSensorPin, INPUT_PULLUP);
+  pinMode(ballSensor, INPUT_PULLUP);
+  pinMode(retrieveSensor, INPUT_PULLUP);
+
+  pinMode(motorRotate, OUTPUT);
+  pinMode(motorFast, OUTPUT);
+  pinMode(wheelLifter, OUTPUT);
+  pinMode(ballFan, OUTPUT);
+  Serial.begin(9600);
+  setWheel(&wheelControl);
+}
+
+void loop() {
+  resetRoundID();
+  playRound();
+}
+
+void playRound(){
+  gameRound newGameRound = {
+    .wheelSectorCounter = 0,
+    .ballLightCount = 0,
+    .sectorLengthCounter = 0,
+    .isSectorCounted = false,
+    .statusWinSensor = false,
+    .statusWheelSensor = false,
+    .statusControlSensor = false,
+    .isNumberRead = false,
+    .winningSector = -1,
+    .winningSectorCorrectCount = 0,
+    .winningNumber = -1,
+    .lastReadNumber = -1,
+  };
+  sendEvent(SNRS, -1);
+  spinSlow(&wheelControl);
+  randomDelay(minPreBetDelay, maxPreBetDelay);
+  sendEvent(SBTS, -1);
+  bettingTimeFunc(&wheelControl, &newGameRound);
+  sendEvent(SBTE, -1);
+  fireBall(&wheelControl, &newGameRound);
+  shuffleWheel(&wheelControl);
+  result(&newGameRound);
+  if (!isBall()) {
+    sendEvent(SRS, newGameRound.winningNumber);
+  }
+  else {
+    sendError(PSF);
+  }
+  roundCounter++; 
+}
+
+void sendEvent(String event, int result) {
+  Serial.print(roundCounter);
+  Serial.print(";");
+  Serial.print(event);
+  Serial.print(";");
+  if (event == "RESULT" && result != -1) {
+    Serial.print(result);    
+  }
+  Serial.println();
+}
+
+volatile void sendError(String error) {
+  while (true){
+    if (!wheelControl.error){
+      Serial.println(error);
+      wheelControl.error = true;
+      stopWheel(&wheelControl);
+    }
+    if (error != PSF) {
+      break;
+    }
+    singleDelay(1000);
+  }
+  wheelControl.error = false;
+  resetFunc();
+}
+
+void resetRoundID(){
+  if (roundCounter == resetRoundThreshold + 1){
+    resetFunc();  
+  }
+}
+
+int randomTime(int min, int max){
+  return random(min, max + 1);  
+}
+
+void randomDelay(int min, int max){
+  int randTime = random(min, max + 1) * multiplier;
+  unsigned long currentTime = millis();
+  while(currentTime + randTime > millis());  
+}
+
+void singleDelay(long milliseconds){
+  unsigned long currentTime = millis();
+  while(currentTime + milliseconds > millis());
+}
+
+void spinSlow(wheel *wheelControl) {
+  if (wheelControl->isWheelRotating == false) {
+    digitalWrite(motorRotate, HIGH);
+    wheelControl->isWheelRotating = true;  
+  }
+  else{
+    digitalWrite(motorFast, LOW);
+    digitalWrite(motorRotate, HIGH);
+  }
+}
+
+void spinFast(wheel *wheelControl) {
+  if (wheelControl->isWheelRotating == true) {
+    digitalWrite(motorFast, HIGH);  
+  }
+}
+
+void stopWheel(wheel *wheelControl) {
+  if(wheelControl->isWheelRotating == true){
+    digitalWrite(motorRotate, LOW);
+    singleDelay(stopWheelDuration);    
+    wheelControl->isWheelRotating = false;  
+  }
+}
+
+void setWheel (wheel *wheelControl) {
+  while(!digitalRead(retrieveSensor)) {
+    digitalWrite(wheelLifter, HIGH);  
+  }
+  digitalWrite(wheelLifter, LOW);
+  singleDelay(2000);
+  digitalWrite(wheelLifter, HIGH);
+  singleDelay(600);
+  digitalWrite(wheelLifter, LOW);
+}
+
+void wheelUp(wheel *wheelControl){
+  
+  if(wheelControl->isWheelUp == false) {
+    while(!digitalRead(retrieveSensor)) {
+      digitalWrite(wheelLifter, HIGH);  
+    }
+    singleDelay(delayWheelUp);
+    digitalWrite(wheelLifter, LOW);    
+    wheelControl->isWheelUp = true;
+  }
+}
+
+void wheelDown(wheel *wheelControl){
+  if(wheelControl->isWheelUp) {
+    digitalWrite(wheelLifter, HIGH);
+    singleDelay(delayWheelDown);
+    digitalWrite(wheelLifter, LOW);
+    wheelControl->isWheelUp = false;
+  }
+}
+
+void retrieveBall(wheel *wheelControl){
+  stopWheel(wheelControl);
+  wheelUp(wheelControl);
+  singleDelay(keepWheelUpDuration);
+  wheelDown(wheelControl);
+  spinSlow(wheelControl);
+}
+
+void shuffleWheel(wheel *wheelControl){
+  long shuffleDuration = randomTime(minShuffleTime, maxShuffleTime) * shuffleTimeMultiplier;
+  Serial.print("Shuffle randTime: ");
+  Serial.println(shuffleDuration);
+  if (wheelControl->isWheelRotating){
+    digitalWrite(motorFast, HIGH);
+    singleDelay(shuffleDuration);
+    digitalWrite(motorFast, LOW);
+  }
+}
+
+void bettingTimeFunc(wheel *wheelControl, gameRound *gameRound){
+  unsigned long currentTime = millis();
+  retrieveBall(wheelControl);
+  singleDelay(lookingForBallDuration);
+
+  while(bettingTime + currentTime > millis()) {
+    if (isBall() == 0) {
+      roundAttemptsToRetrieve++;    
+      if (roundAttemptsToRetrieve <= 3){
+        bettingTimeFunc(wheelControl, gameRound);
+      }
+      else{
+        sendError(EBNF);
+      }
+    }
+    else {
+      roundAttemptsToRetrieve = 0;
+    }  
+  }
+}
+
+int isBall(){
+  if (digitalRead(ballSensor)) {
+    return 1;  
+  }
+  return 0;
+}
+
+void fireBall(wheel *wheelControl, gameRound *gameRound) {
+  int fireBallDuration = (randomTime(fireBallMinTime, fireBallMaxTime) * 10) + fireBallCompressorTime;
+  Serial.print("Fire randTime: ");
+  Serial.println(fireBallDuration);
+  const unsigned long current_time = millis();
+  while (current_time + fireBallDuration > millis()) {
+    digitalWrite(ballFan, HIGH);
+  }
+  digitalWrite(ballFan, LOW);
+}
+
+void readSector(gameRound *gameRound, unsigned long currentTime){
+  gameRound->isSectorCounted = false;
+  gameRound->sectorLengthCounter = 0;
+  gameRound->ballLightCount = 0;
+  while(digitalRead(wheelSensorPin) == 1);
+  while(digitalRead(wheelSensorPin) == 0){
+    if (currentTime + maxReadNumberTime < millis()){
+      sendError(ECRN);
+    }
+    
+    if(gameRound->sectorLengthCounter > sectorLengthThreshold) {
+      gameRound->wheelSectorCounter = 0;    
+    }
+    
+    
+    if(gameRound->isSectorCounted == false){
+      gameRound->wheelSectorCounter++;
+      gameRound->isSectorCounted = true;
+    }
+
+    while (digitalRead(winSensorPin) == 0){
+      gameRound->ballLightCount++;
+      singleDelay(readSectorDelay);
+    }
+    gameRound->sectorLengthCounter++;      
+  }
+  //Serial.println(gameRound->sectorLengthCounter);
+  //Serial.println(gameRound->wheelSectorCounter);
+  
+}
+
+void readNumber(gameRound *gameRound, unsigned long currentTime){
+  bool successfulRead = false;
+  while (!successfulRead){
+    readSector(gameRound, currentTime);
+    if (gameRound->wheelSectorCounter > 36 && gameRound->wheelSectorCounter % 36 == 0){
+      gameRound->wheelSectorCounter = 36;
+    }
+    if (gameRound->ballLightCount >= 17){
+      Serial.print("Sector: ");
+      Serial.println(gameRound->wheelSectorCounter);
+      Serial.print("Light: ");
+      Serial.println(gameRound->ballLightCount);
+      
+      gameRound->winningNumber = numbersArray[gameRound->wheelSectorCounter];
+      successfulRead = true;
+      if(gameRound->lastReadNumber == -1){
+        gameRound->lastReadNumber = gameRound->winningNumber;
+      }
+    }
+  }
+}
+
+void result(gameRound *gameRound){
+  unsigned long currentTime = millis();
+  while(gameRound->winningSectorCorrectCount < correctCountThreshold){
+    readNumber(gameRound, currentTime);
+    if (gameRound->winningNumber != -1 && gameRound->winningNumber == gameRound->lastReadNumber){
+      gameRound->winningSectorCorrectCount++;
+    }
+    else {
+      gameRound->winningSectorCorrectCount = 0;
+      gameRound->lastReadNumber = -1;
+    }
+  }  
+}
+
+
+
+
+
+volatile void sensorTest(){
+  bool counted = false;
+  int lastPrinted = -1;
+  int prevSector = -1;
+  int sectorCount = -1;
+  bool winner = false;
+
+  int counter = 0;
+  while(true){
+    while(digitalRead(winSensorPin) == 0){
+    counter++;
+    singleDelay(10);
+
+    
+    /*Serial.println(sectorCount);
+    sectorCount = 0;
+
+    int darkLength = 0;
+    while (digitalRead(winSensorPin) == 0) {
+      darkLength++;
+      singleDelay(1);
+    }
+    Serial.println(darkLength);
+    //Serial.println(numbersArray[sectorCount]);
+    if (darkLength > 150) {
+      //Serial.println(numbersArray[sectorCount]);
+      Serial.println(darkLength);
+    }
+    Serial.println("------------------");*/
+    }
+    if(counter > 0){
+      Serial.println(counter);
+    }
+    counter = 0;
+  }
+}
---
+++ Bell_Roulette_White/Bell_roulette_white.h
@@ -0,0 +1,84 @@
+#ifndef BELL_ROULETTE_WHITE
+#define BELL_ROULETTE_WHITE
+
+#include <stdbool.h>
+// Error(E) codes
+#define EBNF "BALL_NOT_FOUND"
+#define ECRN "CAN'T_READ_NUMBER"
+#define EMNS "MOTOR_NOT_SPINNING"
+#define EEM "EVENT MISSING"
+#define PSF "POSSIBLE SENSOR FAULT"
+
+// State(S) codes 
+#define SNRS "NEW_ROUND_START"
+#define SBTS "BETTING_TIME_START"
+#define SBTE "BETTING_TIME_END"
+#define SRS "RESULT"
+
+const unsigned long maxRetrieveTime = 10000;
+const unsigned long maxReadNumberTime = 60000;
+const unsigned long lookingForBallDuration = 10000;
+const unsigned long minShuffleTime = 50;
+const unsigned long maxShuffleTime = 80;
+const int shuffleTimeMultiplier = 100;
+const unsigned int stopWheelDuration = 2900;
+const int sectorLengthThreshold = 4;
+const int readSectorDelay = 10;
+const int fireBallCompressorTime = 1000;
+const int fireBallMinTime = 0;
+const int fireBallMaxTime = 30;
+const unsigned long keepWheelUpDuration = 1800;
+const unsigned long bettingTime = 3000 + lookingForBallDuration + keepWheelUpDuration + stopWheelDuration;
+const int powerOnDelay = 2000;
+const int delayBetweenACmotorRelays = 1000;
+const int delayWheelDown = 500;
+const int delayWheelUp = 100;
+const int emergancyRetrieveDelay = 2000;
+const int checkForBallDelay = 5000;
+const int waitForWheelSpinning = 3000;
+const int minPreBetDelay = 10;
+const int maxPreBetDelay = 25;
+const int multiplier = 100;
+const int resetRoundThreshold = 10000;
+
+const int correctCountThreshold = 2;
+
+const int winSensorPin = 2;
+const int wheelSensorPin = 3;
+const int controlWheelSensorPin = 4;
+const int ballSensor = 5;
+const int retrieveSensor = 6;
+
+const int motorRotate = 9;
+const int motorFast = 10;
+const int ballFan = 11;
+const int wheelLifter = 12;
+
+typedef struct gameRound {
+  int wheelSectorCounter;
+  int ballLightCount;
+  int sectorLengthCounter;
+  bool isSectorCounted;
+  bool statusWinSensor;
+  bool statusWheelSensor;
+  bool statusControlSensor;
+  bool isNumberRead;
+  int winningSector;
+  int winningSectorCorrectCount;
+  int winningNumber;
+  int lastReadNumber;
+} gameRound;
+
+typedef struct wheelControl {
+  bool isWheelRotating;
+  bool isBallThere;
+  bool isWheelUp;
+  bool isRetrieved;
+  bool error;
+} wheel;
+
+const int numbersArray[] = {
+    23, 8, 30, 11, 36, 13, 27, 6, 34, 17, 25, 2, 21, 4, 19, 15, 32, 0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 10
+};
+
+#endif
